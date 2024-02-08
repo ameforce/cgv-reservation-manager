4e386931fb376cc8225e4838dcafdba29738b11c
Jongin Kim <enmsoftware@gmail.com>
2024-02-08 09:53:24 +0900

    chore: Update Test code

--- test.py
+++ test.py
@@ -5,6 +5,11 @@ import time
 def main():
     cgv = CGVReservationManager()
     cgv.get_movie_list()
+    cgv.get_movie_type()
+    cgv.get_region()
+    cgv.get_theater()
+    cgv.get_date()
+    cgv.get_time()
     time.sleep(5000)
 
 
